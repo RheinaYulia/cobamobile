7bb918d749484807f196cc9faaa017c58ddc5f3d
aida <yuaida.12@gmail.com>
2022-06-25 14:38:44 +0700

    collaborator

---
+++ lib/screens/main_menu.dart
@@ -0,0 +1,99 @@
+import 'package:cobamobile/screens/quizz_screen.dart';
+import 'package:cobamobile/ui/shared/color.dart';
+import 'package:flutter/material.dart';
+
+class MainMenu extends StatefulWidget {
+  const MainMenu({Key? key}) : super(key: key);
+
+  @override
+  State<MainMenu> createState() => _MainMenuState();
+}
+
+class _MainMenuState extends State<MainMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColor.primaryColor,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 48.0,
+          horizontal: 12.0,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 300,
+            ),
+            const Center(
+              child: Text(
+                "Quizz App",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 48,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            // SizedBox(
+            //   height: 150,
+            // ),
+            // Container(
+            //   child: TextField(
+            //     decoration: InputDecoration(
+            //       hintText: 'Enter your name',
+            //       border: OutlineInputBorder(
+            //         borderRadius: BorderRadius.circular(10),
+            //       ),
+            //       focusedBorder: OutlineInputBorder(
+            //         borderRadius: BorderRadius.circular(10),
+            //       ),
+            //       hintStyle: TextStyle(color: Colors.white),
+            //     ),
+            //   ),
+            // ),
+
+            Expanded(
+              child: Center(
+                child: RawMaterialButton(
+                  onPressed: () {
+                    //Navigating the the Quizz Screen
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => QuizzScreen(),
+                        ));
+                  },
+                  shape: const StadiumBorder(),
+                  fillColor: AppColor.secondaryColor,
+                  child: const Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0),
+                    child: Text(
+                      "Start the Quizz",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 26.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const Center(
+              child: Text(
+                "Dibuat Sebagai Tugas Akhir Matakuliah Mobile",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
